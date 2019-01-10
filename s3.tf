f7104de21b58366f4c9fd6069e5acb8878543a1d
Neil McBride <Neil.McBride@digital.homeoffice.gov.uk>
2019-01-10 14:06:30 +0000

    Add logging bucket

--- s3.tf
+++ s3.tf
@@ -36,3 +36,59 @@ resource "aws_kms_key" "bucket_key" {
 }
 EOF
 }
+
+resource "aws_s3_bucket" "log_archive_bucket" {
+  bucket = "${var.s3_bucket_name["archive_log"]}"
+  acl    = "${var.s3_bucket_acl["archive_log"]}"
+  region = "${var.region}"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${aws_kms_key.bucket_key.arn}"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    Name = "s3-log-archive-bucket-${local.naming_suffix}"
+  }
+}
+
+resource "aws_s3_bucket_metric" "log_archive_bucket_logging" {
+  bucket = "${var.s3_bucket_name["archive_log"]}"
+  name   = "log_archive_bucket_metric"
+}
+
+resource "aws_s3_bucket" "data_archive_bucket" {
+  bucket = "${var.s3_bucket_name["archive_data"]}"
+  acl    = "${var.s3_bucket_acl["archive_data"]}"
+  region = "${var.region}"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${aws_kms_key.bucket_key.arn}"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  logging {
+    target_bucket = "${aws_s3_bucket.log_archive_bucket.id}"
+    target_prefix = "data_archive_bucket/"
+  }
+
+  tags = {
+    Name = "s3-data-archive-bucket-${local.naming_suffix}"
+  }
+}
