df93f8874318fb5b90cbfae28e5c515104c5c9d3
Neil McBride <Neil.McBride@digital.homeoffice.gov.uk>
2019-01-11 10:51:13 +0000

    Add ops peering

--- variables.tf
+++ variables.tf
@@ -17,10 +17,10 @@ variable "region" {
   default = "eu-west-2"
 }
 
-#variable "vpc_peering_connection_ids" {
-#  description = "List of peering VPC connection ids."
-#  type        = "map"
-#}
+variable "vpc_peering_connection_ids" {
+  description = "List of peering VPC connection ids."
+  type        = "map"
+}
 
 variable "route_table_cidr_blocks" {
   description = "Map of CIDR blocks for the Apps private route table."
