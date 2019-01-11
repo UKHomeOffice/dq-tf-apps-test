7b6bb83421bb5d055b7a66429e59908dbbe39878
Neil McBride <Neil.McBride@digital.homeoffice.gov.uk>
2019-01-11 09:36:19 +0000

    Comment out iam_roles for now

---
+++ outputs.tf
@@ -0,0 +1,37 @@
+output "appsvpc_id" {
+  value = "${aws_vpc.appsvpc.id}"
+}
+
+output "appsvpc_cidr_block" {
+  value = "${var.cidr_block}"
+}
+
+output "appssubnet_cidr_block" {
+  value = "${var.public_subnet_cidr_block}"
+}
+
+output "apps_natgw_id" {
+  value = "${aws_nat_gateway.appsnatgw.id}"
+}
+
+#output "outputs" {
+#  value = {
+#    windows = "${aws_instance.win.*.public_dns}"
+#  }
+#}
+
+#output "ad_subnet_id" {
+#  value = "${aws_subnet.ad_subnet.id}"
+#}
+
+output "log_archive_bucket_id" {
+  value = "${aws_s3_bucket.log_archive_bucket.id}"
+}
+
+output "log_archive_bucket_arn" {
+  value = "${aws_s3_bucket.log_archive_bucket.arn}"
+}
+
+#output "iam_roles" {
+#  value = "${concat(module.data_ingest.iam_roles, module.data_pipeline.iam_roles, module.data_feeds.iam_roles, module.external_tableau.iam_roles, module.internal_tableau.iam_roles, module.gpdb-secondary.iam_roles)}"
+#}
