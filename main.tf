0072df5dbf8a75b23231cc49e3a8d5b1db8cb344
Neil McBride <Neil.McBride@digital.homeoffice.gov.uk>
2019-01-30 10:06:18 +0000

    Add aiports module

--- main.tf
+++ main.tf
@@ -30,6 +30,11 @@ module "lambda" {
   route_table_id                   = "${aws_route_table.apps_route_table.id}"
 }
 
+module "airports_pipeline" {
+  source                           = "git::ssh://git@gitlab.digital.homeoffice.gov.uk:2222/dacc-dq/dq-tf-airports-pipeline.git"
+  kms_key_s3                       = "${aws_kms_key.bucket_key.arn}"
+}
+
 module "external_tableau" {
   source                           = "github.com/UKHomeOffice/dq-tf-external-tableau-test"
   appsvpc_id                       = "${aws_vpc.appsvpc.id}"
