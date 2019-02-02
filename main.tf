3ba645e39b57e760c29656b72da27912ebf84233
Neil McBride <Neil.McBride@digital.homeoffice.gov.uk>
2019-02-02 19:10:08 +0000

    Remove dup resource

--- main.tf
+++ main.tf
@@ -33,10 +33,6 @@ module "lambda" {
   route_table_id                   = "${aws_route_table.apps_route_table.id}"
 }
 
-module "airports_input_pipeline" {
-  source                           = "git::ssh://git@gitlab.digital.homeoffice.gov.uk:2222/dacc-dq/dq-tf-airports-input.git"
-}
-
 module "airports_pipeline" {
   source                           = "git::ssh://git@gitlab.digital.homeoffice.gov.uk:2222/dacc-dq/dq-tf-airports-pipeline.git"
   kms_key_s3                       = "${aws_kms_key.bucket_key.arn}"
