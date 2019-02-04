271b4557a171bcf1802c2917bc14d9cc11306d20
Neil McBride <Neil.McBride@digital.homeoffice.gov.uk>
2019-02-04 18:07:18 +0000

    Pass endpoint to rds lambnda

--- main.tf
+++ main.tf
@@ -54,16 +54,15 @@ module "airports_input_pipeline" {
 }
 
 module "rds_deploy" {
-  source            = "git::ssh://git@gitlab.digital.homeoffice.gov.uk:2222/dacc-dq/dq-tf-rds-deploy.git"
-  lambda_subnet     = "${module.lambda.lambda_subnet}"
-  lambda_subnet_az2 = "${module.lambda.lambda_subnet_az2}"
-  lambda_sgrp       = "${module.lambda.lambda_sgrp}"
-  rds_db_name       = "${var.rds_db_name}"
-  rds_endpoint      = "${var.rds_endpoint}"
-  rds_instance      = "${var.rds_instance}"
-  pipeline_count    = "${var.pipeline_count}"
-  naming_suffix     = "${local.naming_suffix}"
-  namespace         = "${var.naming_suffix}"
+  source                             = "git::ssh://git@gitlab.digital.homeoffice.gov.uk:2222/dacc-dq/dq-tf-rds-deploy.git"
+  lambda_subnet                      = "${module.lambda.lambda_subnet}"
+  lambda_subnet_az2                  = "${module.lambda.lambda_subnet_az2}"
+  lambda_sgrp                        = "${module.lambda.lambda_sgrp}"
+  rds_db_name                        = "${var.rds_db_name}"
+  rds_internal_tableau_endpoint      = "${module.internal_tableau.rds_internal_tableau_endpoint}"
+  pipeline_count                     = "${var.pipeline_count}"
+  naming_suffix                      = "${local.naming_suffix}"
+  namespace                          = "${var.naming_suffix}"
 }
 
 module "external_tableau" {
