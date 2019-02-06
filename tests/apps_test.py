640e5f01536e1be594b3f1c14ad155a5e2302b8a
Pete Grant <pete.grant@digital.homeoffice.gov.uk>
2019-02-06 07:30:55 +0000

    Add missing parameter to tests.

--- tests/apps_test.py
+++ tests/apps_test.py
@@ -30,6 +30,7 @@ class TestE2E(unittest.TestCase):
               ad_aws_ssm_document_name        = "1234"
               #ad_writer_instance_profile_name = "1234"
               naming_suffix                   = "preprod-dq"
+              namespace                       = "preprod"
               #haproxy_private_ip              = "1.2.3.3"
               #haproxy_private_ip2             = "1.2.3.4"
 
