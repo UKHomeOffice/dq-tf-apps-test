b1229b745ac6a5121559ed77d34e867cd394485b
Neil McBride <Neil.McBride@digital.homeoffice.gov.uk>
2019-02-08 17:30:02 +0000

    Use generic term

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ module "airports_pipeline" {
   lambda_subnet_az2                  = "${module.lambda.lambda_subnet_az2}"
   lambda_sgrp                        = "${module.lambda.lambda_sgrp}"
   rds_db_name                        = "${var.rds_db_name}"
-  rds_internal_tableau_address       = "${module.internal_tableau.rds_internal_tableau_address}"
+  rds_address                        = "${module.internal_tableau.rds_internal_tableau_address}"
   pipeline_count                     = "${var.pipeline_count}"
   naming_suffix                      = "${local.naming_suffix}"
   namespace                          = "${var.namespace}"
