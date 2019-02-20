ff8c526041a519887576c779d6a3e407e734c663
Neil McBride <Neil.McBride@digital.homeoffice.gov.uk>
2019-02-20 11:36:45 +0000

    Hook in ACL

--- main.tf
+++ main.tf
@@ -81,6 +81,14 @@ module "oag_transform_pipeline" {
   namespace      = "${var.namespace}"
 }
 
+module "acl_input_pipeline" {
+  source         = "git::ssh://git@gitlab.digital.homeoffice.gov.uk:2222/dacc-dq/dq-tf-acl-input-pipeline.git"
+  kms_key_s3     = "${aws_kms_key.bucket_key.arn}"
+  pipeline_count = "${var.pipeline_count}"
+  naming_suffix  = "${local.naming_suffix}"
+  namespace      = "${var.namespace}"
+}
+
 module "external_tableau" {
   source                = "github.com/UKHomeOffice/dq-tf-external-tableau-test"
   appsvpc_id            = "${aws_vpc.appsvpc.id}"
