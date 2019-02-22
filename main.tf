becbbab1846101e40c08416bcff0ea13108252ba
Neil McBride <Neil.McBride@digital.homeoffice.gov.uk>
2019-02-22 07:32:25 +0000

    record level scoring

--- main.tf
+++ main.tf
@@ -113,6 +113,14 @@ module "api_input_pipeline" {
   namespace      = "${var.namespace}"
 }
 
+module "record_level_score_pipeline" {
+  source         = "git::ssh://git@gitlab.digital.homeoffice.gov.uk:2222/dacc-dq/dq-tf-api-record-level-score-pipeline.git"
+  kms_key_s3     = "${aws_kms_key.bucket_key.arn}"
+  pipeline_count = "${var.pipeline_count}"
+  naming_suffix  = "${local.naming_suffix}"
+  namespace      = "${var.namespace}"
+}
+
 module "external_tableau" {
   source                = "github.com/UKHomeOffice/dq-tf-external-tableau-test"
   appsvpc_id            = "${aws_vpc.appsvpc.id}"
