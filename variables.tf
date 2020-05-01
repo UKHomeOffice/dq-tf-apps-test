cd5d448f1bef2361679a046f063f2b9b95319ea0
Jwanderema <wanderema@hotmail.co.uk>
2020-05-01 13:00:09 +0100

    add haproxy ip var

--- variables.tf
+++ variables.tf
@@ -7,8 +7,8 @@ variable "adminpassword" {}
 variable "ad_aws_ssm_document_name" {}
 variable "ad_writer_instance_profile_name" {}
 variable "naming_suffix" {}
-# variable "haproxy_private_ip" {}
-# variable "haproxy_private_ip2" {}
+variable "haproxy_private_ip" {}
+variable "haproxy_private_ip2" {}
 variable "namespace" {}
 
 variable "ad_sg_cidr_ingress" {
