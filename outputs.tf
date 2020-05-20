e5e6bd5e3365907d0640c529ca46b8faaa58ced3
Thet Naing <thet.naing@digital.homeoffice.gov.uk>
2020-05-20 14:49:52 +0100

    terraform code upgraded to 0.12 with modified test cases

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,29 @@
 output "appsvpc_id" {
-  value = "${aws_vpc.appsvpc.id}"
+  value = aws_vpc.appsvpc.id
 }
 
 output "appsvpc_cidr_block" {
-  value = "${var.cidr_block}"
+  value = var.cidr_block
 }
 
 output "appssubnet_cidr_block" {
-  value = "${var.public_subnet_cidr_block}"
+  value = var.public_subnet_cidr_block
 }
 
 output "apps_natgw_id" {
-  value = "${aws_nat_gateway.appsnatgw.id}"
+  value = aws_nat_gateway.appsnatgw.id
 }
 
 output "ad_subnet_id" {
-  value = "${aws_subnet.ad_subnet.id}"
+  value = aws_subnet.ad_subnet.id
 }
 
 output "log_archive_bucket_id" {
-  value = "${aws_s3_bucket.log_archive_bucket.id}"
+  value = aws_s3_bucket.log_archive_bucket.id
 }
 
 output "log_archive_bucket_arn" {
-  value = "${aws_s3_bucket.log_archive_bucket.arn}"
+  value = aws_s3_bucket.log_archive_bucket.arn
 }
 
 # output "iam_roles" {
@@ -31,11 +31,11 @@ output "log_archive_bucket_arn" {
 # }
 
 output "athena_log_bucket" {
-  value = "${aws_s3_bucket.athena_log_bucket.id}"
+  value = aws_s3_bucket.athena_log_bucket.id
 }
 
 output "aws_bucket_key" {
-  value = "${aws_kms_key.bucket_key.arn}"
+  value = aws_kms_key.bucket_key.arn
 }
 
 # output "dq_pipeline_ops_readwrite_database_name_list" {
@@ -47,9 +47,10 @@ output "aws_bucket_key" {
 # }
 #
 output "dq_pipeline_ops_readwrite_bucket_list" {
-  value = "${var.dq_pipeline_ops_readwrite_bucket_list}"
+  value = var.dq_pipeline_ops_readwrite_bucket_list
 }
 
 output "dq_pipeline_ops_readonly_bucket_list" {
-  value = "${var.dq_pipeline_ops_readonly_bucket_list}"
+  value = var.dq_pipeline_ops_readonly_bucket_list
 }
+
