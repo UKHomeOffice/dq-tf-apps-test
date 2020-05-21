8cfd9598c412b73309433cdbf85a281d9e0a5d5a
Thet Naing <coolteddy@gmail.com>
2020-05-21 16:38:47 +0100

    Tf12 upgrade (#51)
    * terraform reference test
    
    * git reference on internal tableau TF12 branch
    
    * git reference on internal tableau TF12 branch typo fix
    
    * terraform code upgraded to 0.12 with modified test cases
    
    * fix terraform test
    
    * pointing to tableau test repo  master
    
    Co-authored-by: Thet Naing <thet.naing@digital.homeoffice.gov.uk>

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
-provider "aws" {}
+provider "aws" {
+}
 
 locals {
   naming_suffix = "apps-${var.naming_suffix}"
@@ -25,28 +26,29 @@ locals {
 # }
 #
 module "internal_tableau" {
-  source                                = "github.com/UKHomeOffice/dq-tf-internal-tableau-test"
+  source = "github.com/UKHomeOffice/dq-tf-internal-tableau-test"
+
   acp_prod_ingress_cidr                 = "10.5.0.0/16"
-  dq_ops_ingress_cidr                   = "${var.route_table_cidr_blocks["ops_cidr"]}"
+  dq_ops_ingress_cidr                   = var.route_table_cidr_blocks["ops_cidr"]
   dq_internal_dashboard_subnet_cidr     = "10.1.12.0/24"
   dq_internal_dashboard_subnet_cidr_az2 = "10.1.13.0/24"
   peering_cidr_block                    = "10.3.0.0/16"
-  apps_vpc_id                           = "${aws_vpc.appsvpc.id}"
-  route_table_id                        = "${aws_route_table.apps_route_table.id}"
-  az                                    = "${var.az}"
-  az2                                   = "${var.az2}"
-  naming_suffix                         = "${local.naming_suffix}"
-  s3_archive_bucket_name                = "${aws_s3_bucket.data_archive_bucket.id}"
-  s3_archive_bucket                     = "${aws_s3_bucket.data_archive_bucket.arn}"
-  s3_archive_bucket_key                 = "${aws_kms_key.bucket_key.arn}"
-  haproxy_private_ip                    = "${var.haproxy_private_ip}"
-  environment                           = "${var.namespace}"
-  s3_httpd_config_bucket                = "${var.s3_httpd_config_bucket}"
-  s3_httpd_config_bucket_key            = "${var.s3_httpd_config_bucket_key}"
-  security_group_ids                    = "${module.lambda.lambda_sgrp}"
-  lambda_subnet                         = "${module.lambda.lambda_subnet}"
-  lambda_subnet_az2                     = "${module.lambda.lambda_subnet_az2}"
-  rds_enhanced_monitoring_role          = "${aws_iam_role.rds_enhanced_monitoring_role.arn}"
+  apps_vpc_id                           = aws_vpc.appsvpc.id
+  route_table_id                        = aws_route_table.apps_route_table.id
+  az                                    = var.az
+  az2                                   = var.az2
+  naming_suffix                         = local.naming_suffix
+  s3_archive_bucket_name                = aws_s3_bucket.data_archive_bucket.id
+  s3_archive_bucket                     = aws_s3_bucket.data_archive_bucket.arn
+  s3_archive_bucket_key                 = aws_kms_key.bucket_key.arn
+  haproxy_private_ip                    = var.haproxy_private_ip
+  environment                           = var.namespace
+  s3_httpd_config_bucket                = var.s3_httpd_config_bucket
+  s3_httpd_config_bucket_key            = var.s3_httpd_config_bucket_key
+  security_group_ids                    = module.lambda.lambda_sgrp
+  lambda_subnet                         = module.lambda.lambda_subnet
+  lambda_subnet_az2                     = module.lambda.lambda_subnet_az2
+  rds_enhanced_monitoring_role          = aws_iam_role.rds_enhanced_monitoring_role.arn
 }
 
 # module "data_feeds" {
@@ -87,15 +89,17 @@ module "internal_tableau" {
 # }
 #
 module "lambda" {
-  source                    = "github.com/ukhomeoffice/dq-tf-lambda"
-  appsvpc_id                = "${aws_vpc.appsvpc.id}"
+  source                    = "git::https://github.com/ukhomeoffice/dq-tf-lambda.git?ref=TF12-upgrade"
+
+  appsvpc_id                = aws_vpc.appsvpc.id
   dq_lambda_subnet_cidr     = "10.1.42.0/24"
   dq_lambda_subnet_cidr_az2 = "10.1.43.0/24"
-  az                        = "${var.az}"
-  az2                       = "${var.az2}"
-  naming_suffix             = "${local.naming_suffix}"
-  route_table_id            = "${aws_route_table.apps_route_table.id}"
+  az                        = var.az
+  az2                       = var.az2
+  naming_suffix             = local.naming_suffix
+  route_table_id            = aws_route_table.apps_route_table.id
 }
+
 #
 # module "airports_pipeline" {
 #   source            = "git::ssh://git@gitlab.digital.homeoffice.gov.uk:2222/dacc-dq/dq-tf-airports-pipeline.git"
@@ -263,15 +267,18 @@ module "lambda" {
 # }
 #
 module "mds_extractor" {
-  source            = "git::ssh://git@gitlab.digital.homeoffice.gov.uk:2222/dacc-dq/dq-tf-mds-extractor.git"
-  lambda_subnet     = "${module.lambda.lambda_subnet}"
-  lambda_subnet_az2 = "${module.lambda.lambda_subnet_az2}"
-  lambda_sgrp       = "${module.lambda.lambda_sgrp}"
+  source            = "git::ssh://git@gitlab.digital.homeoffice.gov.uk:2222/dacc-dq/dq-tf-mds-extractor.git?ref=TF12-upgrade"
+
+  lambda_subnet     = module.lambda.lambda_subnet
+  lambda_subnet_az2 = module.lambda.lambda_subnet_az2
+  lambda_sgrp       = module.lambda.lambda_sgrp
+
   # server            = "${module.data_ingest.rds_mds_address}"
-  kms_key_s3    = "${aws_kms_key.bucket_key.arn}"
-  naming_suffix = "${local.naming_suffix}"
-  namespace     = "${var.namespace}"
+  kms_key_s3    = aws_kms_key.bucket_key.arn
+  naming_suffix = local.naming_suffix
+  namespace     = var.namespace
 }
+
 #
 # module "raw_file_index" {
 #   source            = "git::ssh://git@gitlab.digital.homeoffice.gov.uk:2222/dacc-dq/dq-tf-raw-file-index.git"
@@ -287,20 +294,21 @@ module "mds_extractor" {
 # }
 #
 module "fms" {
-  source     = "github.com/ukhomeoffice/dq-tf-fms"
-  appsvpc_id = "${aws_vpc.appsvpc.id}"
+  source     = "git::https://github.com/ukhomeoffice/dq-tf-fms.git?ref=TF12-upgrade"
 
-  opssubnet_cidr_block = "${var.route_table_cidr_blocks["ops_cidr"]}"
+  appsvpc_id = aws_vpc.appsvpc.id
+
+  opssubnet_cidr_block = var.route_table_cidr_blocks["ops_cidr"]
   fms_cidr_block       = "10.1.40.0/24"
   fms_cidr_block_az2   = "10.1.41.0/24"
   peering_cidr_block   = "10.3.0.0/16"
 
-  az                           = "${var.az}"
-  az2                          = "${var.az2}"
-  naming_suffix                = "${local.naming_suffix}"
-  route_table_id               = "${aws_route_table.apps_route_table.id}"
-  rds_enhanced_monitoring_role = "${aws_iam_role.rds_enhanced_monitoring_role.arn}"
-  environment                  = "${var.namespace}"
+  az                           = var.az
+  az2                          = var.az2
+  naming_suffix                = local.naming_suffix
+  route_table_id               = aws_route_table.apps_route_table.id
+  rds_enhanced_monitoring_role = aws_iam_role.rds_enhanced_monitoring_role.arn
+  environment                  = var.namespace
 }
 
 # module "ops_pipeline" {
@@ -345,52 +353,52 @@ module "fms" {
 # }
 
 resource "aws_vpc" "appsvpc" {
-  cidr_block           = "${var.cidr_block}"
+  cidr_block           = var.cidr_block
   enable_dns_hostnames = true
 
-  tags {
+  tags = {
     Name = "vpc-${local.naming_suffix}"
   }
 }
 
 resource "aws_route_table" "apps_route_table" {
-  vpc_id = "${aws_vpc.appsvpc.id}"
+  vpc_id = aws_vpc.appsvpc.id
 
-  tags {
+  tags = {
     Name = "route-table-${local.naming_suffix}"
   }
 }
 
 resource "aws_route" "ops" {
-  route_table_id            = "${aws_route_table.apps_route_table.id}"
-  destination_cidr_block    = "${var.route_table_cidr_blocks["ops_cidr"]}"
-  vpc_peering_connection_id = "${var.vpc_peering_connection_ids["peering_to_ops"]}"
+  route_table_id            = aws_route_table.apps_route_table.id
+  destination_cidr_block    = var.route_table_cidr_blocks["ops_cidr"]
+  vpc_peering_connection_id = var.vpc_peering_connection_ids["peering_to_ops"]
 }
 
 resource "aws_route" "peering" {
-  route_table_id            = "${aws_route_table.apps_route_table.id}"
-  destination_cidr_block    = "${var.route_table_cidr_blocks["peering_cidr"]}"
-  vpc_peering_connection_id = "${var.vpc_peering_connection_ids["peering_to_peering"]}"
+  route_table_id            = aws_route_table.apps_route_table.id
+  destination_cidr_block    = var.route_table_cidr_blocks["peering_cidr"]
+  vpc_peering_connection_id = var.vpc_peering_connection_ids["peering_to_peering"]
 }
 
 resource "aws_route" "nat" {
-  route_table_id         = "${aws_route_table.apps_route_table.id}"
+  route_table_id         = aws_route_table.apps_route_table.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = "${aws_nat_gateway.appsnatgw.id}"
+  nat_gateway_id         = aws_nat_gateway.appsnatgw.id
 }
 
 resource "aws_route_table" "apps_public_route_table" {
-  vpc_id = "${aws_vpc.appsvpc.id}"
+  vpc_id = aws_vpc.appsvpc.id
 
-  tags {
+  tags = {
     Name = "public-route-table-${local.naming_suffix}"
   }
 }
 
 resource "aws_route" "igw" {
-  route_table_id         = "${aws_route_table.apps_public_route_table.id}"
+  route_table_id         = aws_route_table.apps_public_route_table.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.AppsRouteToInternet.id}"
+  gateway_id             = aws_internet_gateway.AppsRouteToInternet.id
 }
 
 resource "aws_eip" "appseip" {
@@ -398,40 +406,40 @@ resource "aws_eip" "appseip" {
 }
 
 resource "aws_nat_gateway" "appsnatgw" {
-  allocation_id = "${aws_eip.appseip.id}"
-  subnet_id     = "${aws_subnet.public_subnet.id}"
+  allocation_id = aws_eip.appseip.id
+  subnet_id     = aws_subnet.public_subnet.id
 
-  tags {
+  tags = {
     Name = "natgw-${local.naming_suffix}"
   }
 }
 
 resource "aws_internet_gateway" "AppsRouteToInternet" {
-  vpc_id = "${aws_vpc.appsvpc.id}"
+  vpc_id = aws_vpc.appsvpc.id
 
-  tags {
+  tags = {
     Name = "igw-${local.naming_suffix}"
   }
 }
 
 resource "aws_subnet" "public_subnet" {
-  vpc_id                  = "${aws_vpc.appsvpc.id}"
-  cidr_block              = "${var.public_subnet_cidr_block}"
+  vpc_id                  = aws_vpc.appsvpc.id
+  cidr_block              = var.public_subnet_cidr_block
   map_public_ip_on_launch = false
-  availability_zone       = "${var.az}"
+  availability_zone       = var.az
 
-  tags {
+  tags = {
     Name = "public-subnet-${local.naming_suffix}"
   }
 }
 
 resource "aws_route_table_association" "public_subnet" {
-  subnet_id      = "${aws_subnet.public_subnet.id}"
-  route_table_id = "${aws_route_table.apps_public_route_table.id}"
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.apps_public_route_table.id
 }
 
 resource "aws_default_security_group" "default" {
-  vpc_id = "${aws_vpc.appsvpc.id}"
+  vpc_id = aws_vpc.appsvpc.id
 
   egress {
     from_port   = 0
