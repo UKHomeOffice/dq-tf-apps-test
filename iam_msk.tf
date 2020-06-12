c55791fda44574db2a53981f0913e56a863dd67d
Jwanderema <wanderema@hotmail.co.uk>
2020-06-12 16:13:33 +0100

    create user

--- iam_msk.tf
+++ iam_msk.tf
@@ -2,6 +2,7 @@ resource "aws_iam_user" "api_cdlz_msk_bucket" {
   name = "api_cdlz_msk_bucket_user"
 }
 
+
 resource "aws_iam_access_key" "api_cdlz_msk_bucket" {
   user = aws_iam_user.api_cdlz_msk_bucket.name
 }
@@ -14,43 +15,50 @@ resource "aws_iam_group_policy" "api_cdlz_msk_bucket" {
   group = aws_iam_group.api_cdlz_msk_bucket.id
 
   policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "ListS3Bucket",
-      "Effect": "Allow",
-      "Action": [
-        "s3:GetBucketLocation",
-        "s3:GetObject",
-        "s3:ListBucket",
-        "s3:ListBucketMultipartUploads",
-        "s3:ListMultipartUploadParts"
-      ],
-      "Resource": "${aws_s3_bucket.api_cdlz_msk_bucket.arn}"
-    },
-    {
-      "Sid": "PutS3Bucket",
-      "Effect": "Allow",
-      "Action": [
-        "s3:PutObject"
-      ],
-      "Resource": "${aws_s3_bucket.api_cdlz_msk_bucket.arn}/*",
-    },
-    {
-      "Sid": "UseKMSKey",
-      "Effect": "Allow",
-      "Action": [
-        "kms:Encrypt",
-        "kms:Decrypt",
-        "kms:ReEncrypt*",
-        "kms:GenerateDataKey*",
-        "kms:DescribeKey"
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "ListS3Bucket",
+        "Effect": "Allow",
+        "Action": [
+                  "s3:GetBucketLocation",
+                  "s3:GetObject",
+                  "s3:ListBucket",
+                  "s3:ListBucketMultipartUploads",
+                  "s3:ListMultipartUploadParts"
         ],
-        "Resource": "${aws_kms_key.bucket_key.arn}"
-    }
-  ]
-}
+        "Resource": [
+          "${aws_s3_bucket.api_cdlz_msk_bucket.arn}"
+        ]
+      },
+      {
+        "Sid": "PutS3Bucket",
+        "Effect": "Allow",
+        "Action": [
+          "s3:PutObject"
+        ],
+        "Resource": [
+          "${aws_s3_bucket.api_cdlz_msk_bucket.arn}/*"
+        ]
+      },
+      {
+        "Sid": "UseKMSKey",
+        "Effect": "Allow",
+        "Action": [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+          ],
+          "Resource": [
+            "${aws_s3_bucket.api_cdlz_msk_bucket.arn}/*"
+          ]
+      }
+    ]
+  }
+
 EOF
 
 }
