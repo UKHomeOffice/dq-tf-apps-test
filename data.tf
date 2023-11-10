779d92ebc197238a49fbe54bddf06889ea9990e9
Mustafa Deerow <mdeerow@hodqadms-MacBook-Pro-4.local>
2023-11-10 10:19:02 +0000

    dont want it to search for ami in test

--- data.tf
+++ data.tf
@@ -1,37 +1,37 @@
-data "aws_ami" "win" {
-  most_recent = true
+# data "aws_ami" "win" {
+#   most_recent = true
 
-  filter {
-    name = "name"
+#   filter {
+#     name = "name"
 
-    values = [
-      "Windows_Server-2012-R2_RTM-English-64Bit-Base-*",
-    ]
-  }
+#     values = [
+#       "Windows_Server-2012-R2_RTM-English-64Bit-Base-*",
+#     ]
+#   }
 
-  owners = [
-    "amazon",
-  ]
-}
+#   owners = [
+#     "amazon",
+#   ]
+# }
 
 data "aws_availability_zones" "available" {
 }
 
-data "aws_ami" "rhel" {
-  most_recent = true
+# data "aws_ami" "rhel" {
+#   most_recent = true
 
-  filter {
-    name = "name"
+#   filter {
+#     name = "name"
 
-    values = [
-      "RHEL-7.4*",
-    ]
-  }
+#     values = [
+#       "RHEL-7.4*",
+#     ]
+#   }
 
-  owners = [
-    "309956199498",
-  ]
-}
+#   owners = [
+#     "309956199498",
+#   ]
+# }
 
 data "aws_caller_identity" "current" {
 }
