060ca1e6d9ad5a1d2ab563c82c27df4c6f3d51a6
Gurukishore Bolisetty <gurukishore.bolisetty1@digital.homeoffice.gov.uk>
2024-10-04 12:16:39 +0100

    updated main,tf

--- main.tf
+++ main.tf
@@ -304,30 +304,30 @@ module "fms" {
 
   opssubnet_cidr_block = var.route_table_cidr_blocks["ops_cidr"]
   fms_cidr_block       = "10.1.40.0/24"
-  #fms_cidr_block_az2   = "10.1.41.0/24"
-  #peering_cidr_block   = "10.3.0.0/16"
+  fms_cidr_block_az2   = "10.1.41.0/24"
+  peering_cidr_block   = "10.3.0.0/16"
 
-  #az                           = var.az
-  #az2                          = var.az2
-  #naming_suffix                = local.naming_suffix
-  #route_table_id               = aws_route_table.apps_route_table.id
-  #environment                  = var.namespace
+  az                           = var.az
+  az2                          = var.az2
+  naming_suffix                = local.naming_suffix
+  route_table_id               = aws_route_table.apps_route_table.id
+  environment                  = var.namespace
 }
 
-module "ops_pipeline" {
-  source                                = "git::ssh://git@gitlab.digital.homeoffice.gov.uk:2222/dacc-dq/dq-tf-pipeline-ops.git"
-  kms_key_s3                            = aws_kms_key.bucket_key.arn
-  lambda_subnet                         = module.lambda.lambda_subnet
-  lambda_subnet_az2                     = module.lambda.lambda_subnet_az2
-  lambda_sgrp                           = module.lambda.lambda_sgrp
-  rds_internal_tableau_address          = module.internal_tableau.rds_internal_tableau_address
-  rds_fms_address                       = module.fms.rds_address
-  rds_datafeed_address                  = module.data_feeds.rds_address
-  naming_suffix                         = local.naming_suffix
-  namespace                             = var.namespace
-  athena_maintenance_bucket             = var.athena_maintenance_bucket
-  dq_pipeline_ops_readwrite_bucket_list = var.dq_pipeline_ops_readwrite_bucket_list
-}
+#module "ops_pipeline" {
+#  source                                = "git::ssh://git@gitlab.digital.homeoffice.gov.uk:2222/dacc-dq/dq-tf-pipeline-ops.git"
+#  kms_key_s3                            = aws_kms_key.bucket_key.arn
+#  lambda_subnet                         = module.lambda.lambda_subnet
+#  lambda_subnet_az2                     = module.lambda.lambda_subnet_az2
+#  lambda_sgrp                           = module.lambda.lambda_sgrp
+#  rds_internal_tableau_address          = module.internal_tableau.rds_internal_tableau_address
+#  rds_fms_address                       = module.fms.rds_address
+#  rds_datafeed_address                  = module.data_feeds.rds_address
+#  naming_suffix                         = local.naming_suffix
+#  namespace                             = var.namespace
+#  athena_maintenance_bucket             = var.athena_maintenance_bucket
+#  dq_pipeline_ops_readwrite_bucket_list = var.dq_pipeline_ops_readwrite_bucket_list
+#}
 
 #module "dailytasks" {
 #  source        = "github.com/UKHomeOffice/dq-tf-dailytasks"
