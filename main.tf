4e7cff6dc11876ea516793927fba67253e9d8af7
Gurukishore Bolisetty <gurukishore.bolisetty1@digital.homeoffice.gov.uk>
2024-10-04 12:25:41 +0100

    testing

--- main.tf
+++ main.tf
@@ -297,22 +297,22 @@ module "acl_input_pipeline" {
 #  namespace         = var.namespace
 #}
 #
-module "fms" {
-  # contains attribute deprecation in resource "random_string" "username"
-  source     = "github.com/UKHomeOffice/dq-tf-fms-test"
-  appsvpc_id = aws_vpc.appsvpc.id
-
-  opssubnet_cidr_block = var.route_table_cidr_blocks["ops_cidr"]
-  fms_cidr_block       = "10.1.40.0/24"
-  fms_cidr_block_az2   = "10.1.41.0/24"
-  peering_cidr_block   = "10.3.0.0/16"
-
-  az                           = var.az
-  az2                          = var.az2
-  naming_suffix                = local.naming_suffix
-  route_table_id               = aws_route_table.apps_route_table.id
-  environment                  = var.namespace
-}
+#module "fms" {
+#  # contains attribute deprecation in resource "random_string" "username"
+#  source     = "github.com/UKHomeOffice/dq-tf-fms-test"
+#  appsvpc_id = aws_vpc.appsvpc.id
+#
+#  opssubnet_cidr_block = var.route_table_cidr_blocks["ops_cidr"]
+#  fms_cidr_block       = "10.1.40.0/24"
+#  fms_cidr_block_az2   = "10.1.41.0/24"
+#  peering_cidr_block   = "10.3.0.0/16"
+#
+#  az                           = var.az
+#  az2                          = var.az2
+#  naming_suffix                = local.naming_suffix
+#  route_table_id               = aws_route_table.apps_route_table.id
+#  environment                  = var.namespace
+#}
 
 #module "ops_pipeline" {
 #  source                                = "git::ssh://git@gitlab.digital.homeoffice.gov.uk:2222/dacc-dq/dq-tf-pipeline-ops.git"
